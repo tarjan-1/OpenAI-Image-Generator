1b891a9cb8a892ed862f9b2cb78568ce5f40042c
tarjan-1 <75002462+tarjan-1@users.noreply.github.com>
2022-12-08 08:18:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, ()=>{
+    console.log(`Server is started and running on PORT ${ port }`);
+});
